a9e22ccf5377162132b5e360037308d5ea1e0d6b
Daniel Norio <danieltakasu@gmail.com>
2024-03-24 16:48:20 +0000

    feat(minikube): Initial setup of minikube, ingress and jenkins

---
+++ 1. minikube/commands.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Tested in 2024-03-24
+
+## From: https://github.com/codeedu/wsl2-kubernetes?tab=readme-ov-file
+# Download the latest version of Minikube
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+# Make the binary executable
+chmod +x ./minikube
+# Move the binary to your executable path
+sudo mv ./minikube /usr/local/bin/
+
+minikube start --driver=docker
+minikube delete
+minikube start --driver=docker
+minikube kubectl -- get pods -A
+minikube kubectl -- version --client
+minikube dashboard
+
+## Salvar no ~/.bashrc
+alias kubectl="minikube kubectl --"
+
+## From: https://kubernetes.io/docs/tasks/access-application-cluster/ingress-minikube/
+minikube addons enable ingress
+
+## Uses this to expose minikube ip as localhost
+minikube tunnel
