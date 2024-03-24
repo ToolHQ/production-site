d6136b5922df1627545df15814faade14d388ae7
Daniel Norio <danieltakasu@gmail.com>
2024-03-24 17:52:18 +0000

    feat(minio): Adds not working and out of date minio yaml

---
+++ minio/commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Tested in 2024-03-24
+## From both:
+# https://min.io/docs/minio/kubernetes/upstream/
+# https://gist.github.com/balamurugana/c59e868a36bb8a549fe863d22d6f0678
+
+kubectl apply -f minio-dev.yaml
