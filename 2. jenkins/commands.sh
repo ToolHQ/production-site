1463fd9226ca2a2dd53270b0c823df5b241b6a73
Daniel Norio <danieltakasu@gmail.com>
2024-03-29 17:56:00 -0300

    feat: adds more resources limits, refactors jenkins setup

--- 2. jenkins/commands.sh
+++ 2. jenkins/commands.sh
@@ -4,16 +4,13 @@
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
 helm search repo jenkinsci
-# missing, implicited
-kubectl apply -f jenkins-namespace.yaml
-kubectl apply -f jenkins-volume.yaml
+
+kubectl apply -f jenkins-resources.yaml
 
 ## Workaround described if permission issues
 minikube ssh
 sudo chown -R 1000:1000 /data/jenkins-volume
-exit 
-
-kubectl apply -f jenkins-sa.yaml
+exit
 
 chart=jenkinsci/jenkins
 helm install jenkins -n jenkins -f jenkins-values.yaml $chart
@@ -29,12 +26,3 @@ NODE_PORT=$(kubectl get -n jenkins -o jsonpath=$jsonpath services jenkins)
 jsonpath="{.items[0].status.addresses[0].address}"
 NODE_IP=$(kubectl get nodes -n jenkins -o jsonpath=$jsonpath)
 echo http://$NODE_IP:$NODE_PORT/login
-
-## Temporary
-kubectl -n jenkins port-forward jenkins-0 8080:8080
-
-## Testing ingress
-kubectl apply -f jenkins-ingress.yaml
-
-## Salvar no drivers/etc/hosts do Windows e /etc/hosts do linux
-192.168.49.2 hello-world.info
