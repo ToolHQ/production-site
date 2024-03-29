96f855c3171d43ec4eedb13b265443d28918122a
Daniel Norio <danieltakasu@gmail.com>
2024-03-29 19:43:33 -0300

    feat(postgres): Updates setup

--- 5. postgres/commands.sh
+++ 5. postgres/commands.sh
@@ -2,4 +2,4 @@
 # Tested in 2024-03-24
 ## From: https://www.digitalocean.com/community/tutorials/how-to-deploy-postgres-to-kubernetes-cluster
 kubectl apply -f postgres-resources.yaml
-kubectl port-forward --namespace=postgres deployment/postgres 54322:5432
+kubectl port-forward --namespace=postgres deployment/postgres-deployment 54322:5432
