8f51422b1d617d62e95d680cbe4ce4e3912d1e26
Daniel Norio <danieltakasu@gmail.com>
2024-03-30 14:34:02 -0300

    feat(elk): Initial apm, agent and beats + logstash

--- 9. Elastic Cloud on Kubernetes/commands.sh
+++ 9. Elastic Cloud on Kubernetes/commands.sh
@@ -21,3 +21,21 @@ kubectl apply -f quick-start-kibana.yaml
 
 ## 6. Adding ingress to Kibana:
 kubectl apply -f quick-start-kibana-ingress.yaml
+
+## 7. To deploy an APM Server and connect it to the Elasticsearch cluster and Kibana instance you created in the quickstart, apply the following specification:
+kubectl apply -f quick-start-apm-server.yaml
+
+## 8. Adding ingress to APM Server:
+kubectl apply -f quick-start-apm-server-ingress.yaml
+
+## 9. Apply the following specification to deploy Elastic Agent with the System metrics integration to harvest CPU metrics from the Agent Pods:
+kubectl apply -f quick-start-elastic-agent.yaml
+
+## Enterprise only: Deploying Elastic Maps Server can be done with a simple manifest:
+# kubectl apply -f quick-start-elastic-maps.yaml
+
+## 10. Apply the following specification to deploy Filebeat and collect the logs of all containers running in the Kubernetes cluster.
+kubectl apply -f quick-start-beats.yaml
+
+## 11. Add the following specification to create a minimal Logstash deployment that will listen to a Beats agent or Elastic Agent configured to send to Logstash on port 5044, create the service and write the output to an Elasticsearch cluster named quickstart.
+kubectl apply -f quick-start-logstash.yaml
