c5e01116458f8663ba25f0990a9b3832104e4b00
Daniel Norio <danieltakasu@gmail.com>
2024-05-11 20:29:38 -0300

    feat(nginx): Final adjustments, adds k8s resources

--- 7. application/nginx/entrypoint.sh
+++ 7. application/nginx/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-DEFAULT_HTTP_X_FORWARDED_FOR='$http_x_forwarded_for'
+DEFAULT_SOURCE_IP='$http_x_forwarded_for'
+DEFAULT_DNS_RESOLVER=127.0.0.11
 
 # Check if FORCES_IP is set (for geoip testing at local environments)
 if [ "$FORCES_IP" = true ]; then
@@ -10,16 +11,21 @@ fi
 
 # If still empty, set the default value
 if [ -z "$SOURCE_IP" ]; then
-  SOURCE_IP="$DEFAULT_HTTP_X_FORWARDED_FOR"
+  SOURCE_IP="$DEFAULT_SOURCE_IP"
 else
   echo "Initializing with fixed ip: $SOURCE_IP"
 fi
 
 export SOURCE_IP
 
+if [ -z "$SOURCE_IP" ]; then
+  DNS_RESOLVER="$DEFAULT_DNS_RESOLVER"
+fi
+echo "Initializing with DNS Resolver: $DNS_RESOLVER"
+
 find /etc/nginx -type f -name "*.template" | while read -r template_file; do
   echo "Processing file $template_file"
-  envsubst '$STATIC_SERVICE,$STATIC_PROXY_HOST_HEADER,$SOURCE_IP' < "$template_file" > "${template_file%.template}"
+  envsubst '$STATIC_SERVICE,$STATIC_PROXY_HOST_HEADER,$SOURCE_IP,$DNS_RESOLVER' < "$template_file" > "${template_file%.template}"
 done
 
 nginx -t
