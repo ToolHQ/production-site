4118b876880dcaa7ff11ea5794ddaa8a6c69dc0f
Daniel Norio <danieltakasu@gmail.com>
2024-05-11 19:28:29 -0300

    feat(nginx): Adds compose and template the files, improves geoip testing

---
+++ 7. application/nginx/publish.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+DOCKER_REGISTRY_HOST=docker-nexus.localhost
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-nginx:1.0.0
+docker build -t $DOCKER_TAG .
+docker push $DOCKER_TAG
