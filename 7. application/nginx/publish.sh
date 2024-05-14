bb414e48a278017d1d76af316ca04f6a92d1cd31
Daniel Norio <danieltakasu@gmail.com>
2024-05-14 00:14:32 +0000

    feat(app): Convert base front-end to typescript

--- 7. application/nginx/publish.sh
+++ 7. application/nginx/publish.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-nginx:1.0.3
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-nginx:1.0.4
 docker build -t $DOCKER_TAG .
 docker push $DOCKER_TAG
