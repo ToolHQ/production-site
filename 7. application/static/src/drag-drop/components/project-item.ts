31b8546e4bfb9abcaee8825213b7068223772655
Daniel Norio <danieltakasu@gmail.com>
2024-05-16 01:48:01 -0300

    feat(front-end): Separate into modules

---
+++ 7. application/static/src/drag-drop/components/project-item.ts
@@ -0,0 +1,46 @@
+/// <reference path="./base-component.ts" />
+/// <reference path="../decorators/autobind.ts" />
+/// <reference path="../models/project.ts" />
+/// <reference path="../models/drag-drop.ts" />
+
+namespace App {
+  // ProjectItem Class
+  export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements Draggable {
+
+    get persons() {
+      return this.project.people === 1
+        ? '1 person'
+        : `${this.project.people} persons`;
+    }
+
+    constructor(hostId: string, private project: Project) {
+      super('single-project', hostId, false, project.id);
+      this.configure();
+      this.renderContent();
+    }
+
+    @autobind
+    dragStartHandler(event: DragEvent): void {
+      if (event.dataTransfer) {
+        event.dataTransfer.setData('text/plain', this.project.id);
+        event.dataTransfer.effectAllowed = 'move';  
+      }
+    }
+
+    @autobind
+    dragEndHandler(_: DragEvent): void {
+      console.log('DragEnd');
+    }
+
+    configure(): void {
+      this.element.addEventListener('dragstart', this.dragStartHandler);
+      this.element.addEventListener('dragend', this.dragEndHandler);
+    }
+
+    renderContent(): void {
+      this.element.querySelector('h2')!.textContent = this.project.title;
+      this.element.querySelector('h3')!.textContent = `${this.persons} assigned`;
+      this.element.querySelector('p')!.textContent = this.project.description;
+    }
+  }
+}
