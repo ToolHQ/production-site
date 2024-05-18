d91660c3fa0c16aec5c20ac4a03babe359ccf6f9
Daniel Norio <danieltakasu@gmail.com>
2024-05-18 20:31:18 -0300

    feat(front-end): Adding todo list delete logic and styles

--- 7. application/react-static/src/App.tsx
+++ 7. application/react-static/src/App.tsx
@@ -4,23 +4,33 @@ import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 import { Todo } from './todo.model';
 
+let id = 0
+
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos((prevTodos) => [
-      ...prevTodos, 
-      {
-        id: `t${prevTodos.length++}`,
+    setTodos((prevTodos) => {
+      const newTodo = {
+        id: `t${++id}`,
         text
       }
-    ]);
+      const newTodos = [
+        ...prevTodos.filter(Boolean),
+        newTodo
+      ]
+      return newTodos;
+    });
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter(todo => todo.id !== todoId));
   };
 
   return (
     <div className='App'>
       <NewTodo onAddTodo={todoAddHandler}/>
-      <TodoList items={todos}/>
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 }
