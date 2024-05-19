21e7a94f6510777c0f4d5dee63cc24b98e47236a
Daniel Norio <danieltakasu@gmail.com>
2024-05-19 20:17:05 +0000

    feat(back-end): Enhances project seutup, adds k8s files

--- 7. application/back-end/src/app.ts
+++ 7. application/back-end/src/app.ts
@@ -8,6 +8,12 @@ const port = 3000;
 app.use(express.json());
 app.use('/todos', todoRoutes);
 
+const healthCheck: express.RequestHandler<void> = (_, res, _2) => {
+  res.status(200).json({ message: 'Hello World!' });
+}
+
+app.use('/health', healthCheck)
+
 app.use((error: Error, _: Request, res: Response, _2: NextFunction): void => {
   res.status(500).json({ message: error.message });
 })
