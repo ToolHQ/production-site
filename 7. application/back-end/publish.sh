c0d8ae8d2d8e4af600922f9df42ac179e7a1741e
Daniel Norio <danieltakasu@gmail.com>
2024-05-27 12:50:44 +0000

    feat(back-end): Updates dockerfile and publish.sh to nexus.localhost be accessible (support to docker-nexus.localhost is pending)

--- 7. application/back-end/publish.sh
+++ 7. application/back-end/publish.sh
@@ -1,5 +1,18 @@
 #! /bin/sh
+
+# Remove existing Buildx instance
+docker buildx rm minikubebuilder
+
+# Create a new Buildx instance
+docker buildx create --name minikubebuilder --driver docker-container --driver-opt network=minikube
+
+# Use the new Buildx instance
+docker buildx use minikubebuilder
+
+# Define Docker tag
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:1.0.0
-docker build -t $DOCKER_TAG .
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:1.0.1
+
+# Build the Docker image using Buildx
+docker buildx build --load --add-host=docker-nexus.localhost:192.168.49.2 --add-host=nexus.localhost:192.168.49.2 -t $DOCKER_TAG .
 docker push $DOCKER_TAG
