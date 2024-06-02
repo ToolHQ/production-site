342a4b3d35f307577d53ba6a72ad3ce7c43dbc50
Daniel Norio <danieltakasu@gmail.com>
2024-06-02 20:59:20 +0000

    feat(back-end): Adds eslint

--- 7. application/back-end/src/app.ts
+++ 7. application/back-end/src/app.ts
@@ -13,12 +13,14 @@ const port = 3000;
 app.use(express.json());
 app.use('/todos', todoRoutes);
 
-const healthCheck: express.RequestHandler<void> = (_, res, _2) => {
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const healthCheck: express.RequestHandler<void> = (_, res) => {
   res.status(200).json({ message: 'Hello World!' });
 };
 
 app.use('/health', healthCheck);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: Error, _: Request, res: Response, _2: NextFunction): void => {
   res.status(500).json({ message: error.message });
 });
@@ -55,7 +57,11 @@ app.use('/test', async (_, res) => {
       'x-ratelimit-reset',
     ],
   });
-  Readable.fromWeb(streamReturn.body!).pipe(res);
+  if (streamReturn.body) {
+    Readable.fromWeb(streamReturn.body).pipe(res);
+  } else {
+    res.end();
+  }
 });
 
 app.listen(port, () => {
