29dfe8cc02c935281aee3ffa9bc49a45c51943d9
Daniel Norio <danieltakasu@gmail.com>
2024-06-02 19:22:21 +0000

    feat(back-end): Initial integration with httpclient

--- 7. application/back-end/src/app.ts
+++ 7. application/back-end/src/app.ts
@@ -1,9 +1,12 @@
+import { Readable } from 'node:stream';
 import Logger from '@dnorio/logger'
+import HttpClient from '@dnorio/httpclient'
 import express, { Request, Response, NextFunction } from 'express';
 
 import todoRoutes from './routes/todo';
 
 const { logger } = Logger();
+const httpClient = HttpClient();
 const app = express();
 const port = 3000;
 
@@ -20,7 +23,19 @@ app.use((error: Error, _: Request, res: Response, _2: NextFunction): void => {
   res.status(500).json({ message: error.message });
 })
 
+app.use('/test', async (_, res) => {
+  logger.info('Hello World!');
+  logger.infoDB({ name: 'John', age: 30 }, 'User created');
+  logger.error('Error message');
+  logger.errorEvent('Error 112', Error('new one'));
+
+  const streamReturn = await httpClient.callHTTP({
+    uri: 'https://jsonplaceholder.typicode.com/todos/1',
+    stream: true
+  });
+  Readable.fromWeb(streamReturn.body!).pipe(res);
+})
+
 app.listen(port, () => {
   logger.infoEvent('Server started', { port });
-  console.error(Error('teste'));
 });
