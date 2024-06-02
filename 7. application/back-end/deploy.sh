eb9156b8a779078dfc19574b835eac1dd2dc40f2
Daniel Norio <danieltakasu@gmail.com>
2024-06-02 19:21:10 +0000

    feat(back-end): Enhances back-end deploy process

--- 7. application/back-end/deploy.sh
+++ 7. application/back-end/deploy.sh
@@ -1,18 +1,20 @@
 #! /bin/sh
 
 # Remove existing Buildx instance
-docker buildx rm minikubebuilder
+# docker buildx rm minikubebuilder
 
 # Create a new Buildx instance
-docker buildx create --name minikubebuilder --driver docker-container --driver-opt network=minikube
+# docker buildx create --name minikubebuilder --driver docker-container --driver-opt network=minikube
 
 # Use the new Buildx instance
-docker buildx use minikubebuilder
+# docker buildx use minikubebuilder
 
 # Define Docker tag
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:1.0.3
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:latest
 
 # Build the Docker image using Buildx
 docker buildx build --load --add-host=docker-nexus.localhost:192.168.49.2 --add-host=nexus.localhost:192.168.49.2 -t $DOCKER_TAG .
-# docker push $DOCKER_TAG
+docker push $DOCKER_TAG
+
+kubectl apply -f ./k8s/minikube/my-site-back-end.yaml
