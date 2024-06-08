7672e96ae1a6a997914372886f38f18499729e2a
Daniel Norio <danieltakasu@gmail.com>
2024-06-08 20:21:02 +0000

    feat(back-end): Better typings to be encapsulated at validations.ts

--- 7. application/back-end/src/routes/integration.ts
+++ 7. application/back-end/src/routes/integration.ts
@@ -1,18 +1,14 @@
-import { RequestHandler, Router } from 'express';
+import { Router } from 'express';
 
 import { testHttp, testDatabase } from '../controllers/integration.js';
 import { validateMiddleware } from '../services/validations.js';
-import { DatabaseConfigParams } from '../types.js';
-
 export const router = Router();
 
 router.get('/http', testHttp);
 
 router.get(
   '/database/:connectionName/metadata',
-  validateMiddleware(
-    'DatabaseConfigParams'
-  ) as unknown as RequestHandler<DatabaseConfigParams>,
+  validateMiddleware('DatabaseConfigParams'),
   testDatabase
 );
 
