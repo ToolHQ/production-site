75ae07ea1f2c72e7274a3eaaf85311022a0245b6
Daniel Norio <danieltakasu@gmail.com>
2024-06-08 21:05:36 +0000

    feat(back-end): Adds better ts dynamic build

--- 7. application/back-end/generate-schema.js
+++ 7. application/back-end/generate-schema.js
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
+import ts from 'typescript';
 import tsj from 'typescript-json-schema';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,24 +16,52 @@ const settings = {
 // Path to the tsconfig file
 const tsconfig = 'tsconfig.json';
 
-// Specify the type(s) you want to generate schema for
-const types = ['DatabaseConfigParams'];
+// Options for the TypeScript compiler
+const compilerOptions = {
+  target: 99,
+  module: 99,
+};
+
+const sourceFile = `${__dirname}/src/types.ts`;
+
+// Create a Program instance
+const program = ts.createProgram([sourceFile], compilerOptions);
+
+// Get all exported interface or type names
+const typeNames = [];
+const sourceFiles = program.getSourceFiles();
+for (const sourceF of sourceFiles) {
+  if (!sourceF.isDeclarationFile) {
+    ts.forEachChild(sourceF, (node) => {
+      if (ts.isInterfaceDeclaration(node) || ts.isTypeAliasDeclaration(node)) {
+        if (
+          node.modifiers &&
+          node.modifiers.some(
+            (modifier) => modifier.kind === ts.SyntaxKind.ExportKeyword
+          )
+        ) {
+          typeNames.push(node.name.escapedText);
+        }
+      }
+    });
+  }
+}
 
 // Generate the schema(s)
-const program = tsj.getProgramFromFiles(
+const tsjProgram = tsj.getProgramFromFiles(
   [`${__dirname}/src/types.ts`],
   tsconfig
 );
-for (const type of types) {
-  const schema = tsj.generateSchema(program, type, settings);
+for (const type of typeNames) {
+  const schema = tsj.generateSchema(tsjProgram, type, settings);
   fs.writeFileSync(`schemas/${type}.json`, JSON.stringify(schema, null, 2));
 }
 
-const exportedSchemasTsContent = `import {${types
+const exportedSchemasTsContent = `import {${typeNames
   .map((type) => `\n  ${type}`)
-  .join(',')}\n} from './types.js';\nexport type ExportedSchemas = ${types
+  .join(',')}\n} from './types.js';\nexport type ExportedSchemas = ${typeNames
   .map((type) => JSON.stringify(type))
-  .join(' | ')};\nexport type SchemaTypes = {${types
+  .join(' | ')};\nexport type SchemaTypes = {${typeNames
   .map((type) => `\n  ${type}: ${type}`)
   .join(',')}\n};\n`;
 fs.writeFileSync(`src/exportedSchemas.ts`, exportedSchemasTsContent);
