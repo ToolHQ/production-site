777a342f39ad1c99a5593177454d2ea0c7926111
Daniel Norio <danieltakasu@gmail.com>
2024-06-16 12:59:16 -0300

    feat(swagger): Constraint swagger entityName for generateMigrationParams

--- 7. application/back-end/generate-schema.js
+++ 7. application/back-end/generate-schema.js
@@ -11,7 +11,7 @@ const __dirname = dirname(__filename);
 // Settings for the JSON schema generation
 const settings = {
   required: true,
-  uniqueNames: true,
+  // uniqueNames: true,
 };
 
 // Path to the tsconfig file
