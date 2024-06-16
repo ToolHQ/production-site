3e7eb998af7cf201981fd63237e6d1c7ccdfef18
Daniel Norio <danieltakasu@gmail.com>
2024-06-16 00:18:29 -0300

    feat(swagger): Adds swagger generation for input bodies

--- 7. application/back-end/src/controllers/todos.ts
+++ 7. application/back-end/src/controllers/todos.ts
@@ -2,10 +2,20 @@ import { RequestHandler } from 'express';
 
 import { Todo } from '../models/todo.js';
 
+import {
+  CreateTodoParams,
+  CreateTodoResponseBody,
+  CreateTodoInputBody,
+} from '../types.js';
+
 const TODOS: Todo[] = [];
 
-export const createTodo: RequestHandler = (req, res) => {
-  const { text } = req.body as { text: string };
+export const createTodo: RequestHandler<
+  CreateTodoParams,
+  CreateTodoResponseBody,
+  CreateTodoInputBody
+> = (req, res) => {
+  const { text } = req.body;
   const newTodo = new Todo(Math.random().toString(), text);
   TODOS.push(newTodo);
   res.status(201).json({ message: 'Created the todo.', createdTodo: newTodo });
--- 7. application/back-end/src/types.ts
+++ 7. application/back-end/src/types.ts
@@ -1,9 +1,20 @@
 // import { rawRequest } from '@dnorio/models-toolhq';
+import { Todo } from './models/todo';
 export interface DatabaseConfigParams {
   connectionName: 'postgres';
   port?: number;
   isActive?: boolean;
   options?: string[];
+}
+
+export type CreateTodoParams = Record<string, never>;
+export interface CreateTodoInputBody {
+  text: string;
+}
+
+export interface CreateTodoResponseBody {
+  message: string;
+  createdTodo: Todo;
 }
 
 export interface GenerateMigrationParams {
