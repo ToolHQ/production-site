5fb687708f180e7b80bbeb8209cb9e8d6284bca2
Daniel Norio <danieltakasu@gmail.com>
2024-08-26 10:02:52 +0000

    feat(postgres): Updates tracking parameters

--- 5. postgres/build.sh
+++ 5. postgres/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/postgres:16.3-alpine3.20-1.0.4
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/postgres:16.3-alpine3.20-1.0.6
 docker build . -t $DOCKER_TAG
 docker push $DOCKER_TAG
 kubectl apply -f ./postgres-resources.yaml
