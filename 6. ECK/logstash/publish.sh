9cb230041d5f212cff8127ffcb4b5dee417fac55
Daniel Norio <danieltakasu@gmail.com>
2025-01-29 02:00:05 +0000

    feat(logs): Updates ECK stack

--- 6. ECK/logstash/publish.sh
+++ 6. ECK/logstash/publish.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/logstash:8.13.3-custom.3
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/logstash:8.17.1-custom.3
 docker build -t $DOCKER_TAG .
 docker push $DOCKER_TAG
