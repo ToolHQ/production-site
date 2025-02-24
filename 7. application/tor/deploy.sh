cd659047d09255a54674f3fbed0319ccc3f2a088
Daniel Norio <danieltakasu@gmail.com>
2025-02-24 07:12:53 -0300

    feat(back): Adding tor deployment, minor adjustments

---
+++ 7. application/tor/deploy.sh
@@ -0,0 +1,20 @@
+#! /bin/sh
+
+# Remove existing Buildx instance
+# docker buildx rm minikubebuilder
+
+# Create a new Buildx instance
+# docker buildx create --name minikubebuilder --driver docker-container --driver-opt network=minikube
+
+# Use the new Buildx instance
+# docker buildx use minikubebuilder
+
+# Define Docker tag
+DOCKER_REGISTRY_HOST=docker-nexus.localhost
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/torproxy:latest
+
+# Build the Docker image using Buildx
+docker buildx build --load --add-host=docker-nexus.localhost:192.168.49.2 --add-host=nexus.localhost:192.168.49.2 --add-host=minio.localhost:192.168.49.2 -t $DOCKER_TAG .
+docker push $DOCKER_TAG
+
+kubectl apply -f ./k8s/minikube/torproxy.yaml
