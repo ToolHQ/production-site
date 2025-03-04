14f9a05fdd90e9358447553ff53a1bf7f5e17845
Daniel Norio <danieltakasu@gmail.com>
2025-03-04 08:51:52 -0300

    feat(apm): TraceIds at logs

--- 7. application/back-end/src/apm.ts
+++ 7. application/back-end/src/apm.ts
@@ -1,25 +1,39 @@
-import ElasticAPM from 'elastic-apm-node';
+import ElasticAPM, { LogLevel } from 'elastic-apm-node';
 
 import Logger from '@dnorio/logger';
 
 const { logger } = Logger();
 
-if (process.env.APM_SERVER_TOKEN) {
-  ElasticAPM.start({
+if (process.env.ELASTIC_APM_SERVER_TOKEN) {
+  const apm = ElasticAPM.start({
     serviceName: 'my-site-back-end',
     verifyServerCert: false,
-    secretToken: process.env.APM_SERVER_TOKEN,
-    serverUrl: process.env.APM_SERVER_URL,
+    secretToken: process.env.ELASTIC_APM_SERVER_TOKEN,
+    serverUrl: process.env.ELASTIC_APM_SERVER_URL,
     captureBody: 'all', // Default: off
-    logLevel: 'info', // Default: info
+    logLevel: (process.env.ELASTIC_APM_LOG_LEVEL as LogLevel) || 'info', // Default: info
     logger: {
       fatal: logger.error,
       warn: logger.warn,
       error: logger.error,
       info: logger.info,
-      debug: logger.debug,
-      trace: logger.debug,
+      debug: logger.info,
+      trace: logger.info,
     },
+    transactionMaxSpans: 1000, // Default: 500
+    spanCompressionEnabled: false, // Default: true
+    ignoreUrls: ['/health'],
+  });
+  Logger.addWrapper((obj: Record<string, unknown>) => {
+    const idsObj = apm.currentTransaction ? apm.currentTransaction.ids : null;
+    if (!idsObj) {
+      return obj;
+    }
+    obj = {
+      ...obj,
+      apm: idsObj,
+    };
+    return obj;
   });
 }
 
