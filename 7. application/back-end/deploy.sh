f4e94449fd1532e0b3bb9716a653b2495d033c60
Daniel Norio <danieltakasu@gmail.com>
2025-03-04 08:32:29 -0300

    feat(back): Better image tagging

--- 7. application/back-end/deploy.sh
+++ 7. application/back-end/deploy.sh
@@ -14,11 +14,17 @@ npm cache clean -f && rm -rf node_modules/ && NODE_TLS_REJECT_UNAUTHORIZED=0 npm
 npm run build
 
 # Define Docker tag
+TAG_VERSION=$(date +%s)
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:latest
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:$TAG_VERSION
+DOCKER_TAG_LATEST=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-back-end:latest
 
 # Build the Docker image using Buildx
 docker buildx build --load --add-host=docker-nexus.localhost:192.168.49.2 --add-host=nexus.localhost:192.168.49.2 --add-host=minio.localhost:192.168.49.2 -t $DOCKER_TAG .
 docker push $DOCKER_TAG
+docker tag $DOCKER_TAG $DOCKER_TAG_LATEST
+docker push $DOCKER_TAG_LATEST
+
+sed -i "s|image: .*|image: $DOCKER_TAG|" ./k8s/minikube/my-site-back-end.yaml
 
 kubectl apply -f ./k8s/minikube/my-site-back-end.yaml
