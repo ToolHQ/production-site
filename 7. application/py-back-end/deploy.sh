7c1ee5bc06986566216c9ba2f6def25fb83460e4
Daniel Norio <danieltakasu@gmail.com>
2025-03-06 08:20:23 -0300

    feat(data): Enhances data testing, adds userProfile and loginAudit

--- 7. application/py-back-end/deploy.sh
+++ 7. application/py-back-end/deploy.sh
@@ -3,8 +3,8 @@
 # Define Docker tag
 TAG_VERSION=$(date +%s)
 DOCKER_REGISTRY_HOST=docker-nexus.localhost
-DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-nginx:$TAG_VERSION
-DOCKER_TAG_LATEST=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-nginx:latest
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-py-back-end:$TAG_VERSION
+DOCKER_TAG_LATEST=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-py-back-end:latest
 
 # Build the Docker image using Buildx
 docker buildx build --load --add-host=docker-nexus.localhost:192.168.49.2 --add-host=nexus.localhost:192.168.49.2 --add-host=minio.localhost:192.168.49.2 -t $DOCKER_TAG .
@@ -12,6 +12,6 @@ docker push $DOCKER_TAG
 docker tag $DOCKER_TAG $DOCKER_TAG_LATEST
 docker push $DOCKER_TAG_LATEST
 
-sed -i "s|image: .*|image: $DOCKER_TAG|" ./k8s/minikube/my-site-nginx.yaml
+sed -i "s|image: .*|image: $DOCKER_TAG|" ./k8s/minikube/my-site-py-back-end.yaml
 
-kubectl apply -f ./k8s/minikube/my-site-nginx.yaml
+kubectl apply -f ./k8s/minikube/my-site-py-back-end.yaml
