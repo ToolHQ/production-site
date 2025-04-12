a2c209336f754eff71abcc7d2b7dbb5c28f14408
Daniel Norio <danieltakasu@gmail.com>
2025-04-12 16:54:06 -0300

    feat(back-end): Adds base rust api

---
+++ 7. application/rs-back-end/deploy.sh
@@ -0,0 +1,17 @@
+#! /bin/sh
+
+# Define Docker tag
+TAG_VERSION=$(date +%s)
+DOCKER_REGISTRY_HOST=docker-nexus.localhost
+DOCKER_TAG=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-rs-back-end:$TAG_VERSION
+DOCKER_TAG_LATEST=$DOCKER_REGISTRY_HOST/repository/docker-repo/my-site-rs-back-end:latest
+
+# Build the Docker image using Buildx
+docker buildx build --load --add-host=docker-nexus.localhost:192.168.49.2 --add-host=nexus.localhost:192.168.49.2 --add-host=minio.localhost:192.168.49.2 -t $DOCKER_TAG .
+docker push $DOCKER_TAG
+docker tag $DOCKER_TAG $DOCKER_TAG_LATEST
+docker push $DOCKER_TAG_LATEST
+
+sed -i "s|image: .*|image: $DOCKER_TAG|" ./k8s/minikube/my-site-rs-back-end.yaml
+
+kubectl apply -f ./k8s/minikube/my-site-rs-back-end.yaml
