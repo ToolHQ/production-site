96de9a10c92b89cfd10e9e9bcc55f788b12d4b47
Daniel Norio <danieltakasu@gmail.com>
2025-05-02 02:48:49 +0000

    feat(data): Experiments raw server trial

---
+++ 7. application/rs-back-end/zero/src/main.rs
@@ -0,0 +1,79 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+fn main() {
+    // Spawn the server
+    // std::thread::spawn(|| {
+    let listener = TcpListener::bind("0.0.0.0:3000").expect("Failed to bind");
+
+    println!("🟢 Listening on http://0.0.0.0:3000");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(|| {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                eprintln!("Connection failed: {}", e);
+            }
+        }
+    }
+    // });
+
+    // Keep the main thread alive
+    // loop {
+    //     std::thread::park();
+    // }
+}
+
+fn close_connection(stream: TcpStream) {
+    if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
+        eprintln!("Failed to close connection: {}", e);
+    }
+}
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    if stream.read(&mut buffer).is_err() {
+        close_connection(stream);
+        return;
+    }
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer));
+    if buffer.starts_with(b"GET /health") {
+        let response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!";
+        let _ = stream.write(response.as_bytes());
+        close_connection(stream);
+        return;
+    }
+
+    if buffer.starts_with(b"GET /ndjson ") {
+        if let Ok(file) = File::open("/app/data/flights-1m.ndjson") {
+            let _ = stream.write_all(
+                b"HTTP/1.1 200 OK\r\nContent-Type: application/x-ndjson\r\nContent-Disposition: attachment; filename=\"file.ndjson\"\r\n\r\n",
+            );
+            let mut line = String::new();
+            let mut reader = BufReader::new(file);
+            loop {
+                line.clear();
+                let bytes = reader.read_line(&mut line).unwrap_or(0);
+                if bytes == 0 {
+                    break;
+                }
+                let _ = stream.write_all(line.as_bytes());
+                let _ = stream.flush();
+            }
+            close_connection(stream);
+        } else {
+            let _ = stream.write_all(b"HTTP/1.1 500 Internal Server Error\r\n\r\nFile not found");
+            close_connection(stream);
+        }
+    } else {
+        let _ = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\nRoute not found");
+        close_connection(stream);
+    }
+}
