6efd6993d9738e75458d78b6fef6cd3535870e60
Daniel Norio <danieltakasu@gmail.com>
2025-05-10 19:20:39 +0000

    feat(rs-vanilla-back-end): Semaphore debug logs

--- apps/rs-vanilla-back-end/src/main.rs
+++ apps/rs-vanilla-back-end/src/main.rs
@@ -20,16 +20,27 @@ impl Semaphore {
     }
 
     fn acquire(&self) {
+        // let thread_id = std::thread::current().id();
         let mut count = self.count.lock().unwrap();
         while *count == 0 {
+            // println!("[{thread_id:?}] 🚫 Semaphore full. Waiting...");
             count = self.condvar.wait(count).unwrap();
         }
         *count -= 1;
+        // println!(
+        // "[{thread_id:?}] ✅ Acquired semaphore. Remaining: {}",
+        //     *count
+        // );
     }
 
     fn release(&self) {
+        // let thread_id = std::thread::current().id();
         let mut count = self.count.lock().unwrap();
         *count += 1;
+        // println!(
+        //     "[{thread_id:?}] 🔓 Released semaphore. Remaining: {}",
+        //     *count
+        // );
         self.condvar.notify_one();
     }
 }
