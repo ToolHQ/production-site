9557b4e13f4389927d1d9c7de92cf20a72225037
Daniel Norio <danieltakasu@gmail.com>
2025-05-10 17:38:39 +0000

    feat(rs-vanilla-back-end): Adds better parsing and handling of http version, http headers and methods

--- apps/rs-vanilla-back-end/src/main.rs
+++ apps/rs-vanilla-back-end/src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read, Write};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
@@ -36,45 +36,155 @@ fn close_connection(stream: TcpStream) {
 }
 
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
-    if stream.read(&mut buffer).is_err() {
-        close_connection(stream);
-        return;
+    let peer_addr = stream.peer_addr().unwrap();
+    let mut buf_reader = BufReader::new(stream.try_clone().unwrap());
+
+    // Loop to support request pipelining
+    loop {
+        let mut request_line = String::new();
+
+        // Read just the first line of the request to get method, path, and version
+        let mut bytes_read = match buf_reader.read_line(&mut request_line) {
+            Ok(n) if n == 0 => {
+                // EOF: client closed connection cleanly
+                close_connection(stream);
+                drop(buf_reader);
+                return;
+            }
+            Ok(n) => n,
+            Err(e) => {
+                eprintln!("Failed to read from {}: {}", peer_addr, e);
+                close_connection(stream);
+                drop(buf_reader);
+                return;
+            }
+        };
+
+        let first_line_parts: Vec<&str> = request_line.split_whitespace().collect();
+        if first_line_parts.len() != 3 {
+            // TODO: Support HTTP/0.9
+            let _ = stream.write_all(b"HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n");
+            eprintln!("Invalid request line: {}", request_line);
+            close_connection(stream);
+            return;
+        }
+        let http_method = first_line_parts[0].to_string();
+        let http_path = first_line_parts[1].to_string();
+        let http_version = first_line_parts[2].to_string();
+
+        if http_version != "HTTP/1.1" {
+            // TODO: Support HTTP/1.0 and HTTP/2 and HTTP/3
+            let _ = stream
+                .write_all(b"HTTP/1.1 505 HTTP Version Not Supported\r\nContent-Length: 0\r\n\r\n");
+            eprintln!("Unsupported HTTP version: {}", http_version);
+            close_connection(stream);
+            drop(buf_reader);
+            return;
+        }
+
+        // Headers of interest
+        let mut connection_header = "keep-alive".to_string();
+
+        // Read line by line to get the headers using O(1) memory
+        loop {
+            request_line.clear();
+            bytes_read += match buf_reader.read_line(&mut request_line) {
+                Ok(n) if n == 0 => {
+                    // EOF: client closed connection cleanly
+                    close_connection(stream);
+                    drop(buf_reader);
+                    return;
+                }
+                Ok(n) => n,
+                Err(e) => {
+                    eprintln!("Failed to read from {}: {}", peer_addr, e);
+                    close_connection(stream);
+                    drop(buf_reader);
+                    return;
+                }
+            };
+
+            let request_line_trimmed = request_line.trim_end();
+            // When \r\n\r\n is reached, the headers are done
+            if request_line_trimmed.is_empty() {
+                break;
+            }
+
+            if let Some((header_name, header_value)) = request_line_trimmed.split_once(':') {
+                let header_name = header_name.trim();
+                let header_value = header_value.trim();
+                if header_name.eq_ignore_ascii_case("Connection") {
+                    connection_header = header_value.to_string();
+                }
+            } else {
+                eprintln!("Invalid header: {}", request_line_trimmed);
+            }
+        }
+
+        // Handles GET /health
+        if http_method.eq_ignore_ascii_case("GET") && http_path == "/health" {
+            let response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+            let _ = stream.write_all(response.as_bytes());
+            if connection_header.eq_ignore_ascii_case("close") {
+                close_connection(stream);
+                break;
+            }
+            continue;
+        }
+
+        // Logs request
+        println!(
+            "Request Received: {} {} from {}. Bytes read: {}. Connection: {}",
+            http_method, http_path, peer_addr, bytes_read, connection_header
+        );
+
+        // Handles GET /ndjson
+        if http_method.eq_ignore_ascii_case("GET") && http_path == "/ndjson" {
+            if let Ok(file) = File::open("/app/data/flights-1m.ndjson") {
+                let _ = stream.write_all(
+                    b"HTTP/1.1 200 OK\r\nContent-Type: application/x-ndjson\r\nContent-Disposition: attachment; filename=\"file.ndjson\"\r\n\r\n",
+                );
+                let mut line = String::new();
+                let mut reader = BufReader::with_capacity(1024 * 10, file);
+                loop {
+                    line.clear();
+                    let bytes = reader.read_line(&mut line).unwrap_or(0);
+                    if bytes == 0 {
+                        break;
+                    }
+                    let _ = stream.write_all(line.as_bytes());
+                    let _ = stream.flush();
+                }
+                drop(line);
+                drop(reader);
+                return;
+            } else {
+                let _ =
+                    stream.write_all(b"HTTP/1.1 500 Internal Server Error\r\n\r\nFile not found");
+                return;
+            }
+        }
+
+        // Send 404 as default
+        let response_body = format!(
+            "You requested {} {}\nFrom {} (bytes read: {})\n",
+            http_method, http_path, peer_addr, bytes_read
+        );
+        let response = format!(
+            "HTTP/1.1 404 Not Found\r\nContent-Length: {}\r\nConnection: {}\r\nContent-Type: text/plain\r\n\r\n{}",
+            response_body.len(),
+            connection_header,
+            response_body
+        );
+        println!("Response {}", response_body);
+        let _ = stream.write_all(response.as_bytes());
+
+        if connection_header.eq_ignore_ascii_case("close") {
+            // If the connection header is "close", close the connection
+            close_connection(stream);
+            break;
+        }
     }
 
-    if buffer.starts_with(b"GET /health") {
-        let response = String::from("HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, world!");
-        if let Err(e) = stream.write_all(response.as_bytes()) {
-            eprintln!("Failed to write (/health): {e}");
-        }
-        drop(response);
-        close_connection(stream);
-        return;
-    }
-    println!("Request: {}", String::from_utf8_lossy(&buffer));
-    if buffer.starts_with(b"GET /ndjson ") {
-        if let Ok(file) = File::open("/app/data/flights-1m.ndjson") {
-            let _ = stream.write_all(
-                b"HTTP/1.1 200 OK\r\nContent-Type: application/x-ndjson\r\nContent-Disposition: attachment; filename=\"file.ndjson\"\r\n\r\n",
-            );
-            let mut line = String::new();
-            let mut reader = BufReader::with_capacity(1024 * 10, file);
-            loop {
-                line.clear();
-                let bytes = reader.read_line(&mut line).unwrap_or(0);
-                if bytes == 0 {
-                    break;
-                }
-                let _ = stream.write_all(line.as_bytes());
-                let _ = stream.flush();
-            }
-            drop(line);
-            drop(reader);
-        } else {
-            let _ = stream.write_all(b"HTTP/1.1 500 Internal Server Error\r\n\r\nFile not found");
-        }
-    } else {
-        let _ = stream.write_all(b"HTTP/1.1 404 Not Found\r\n\r\nRoute not found");
-    }
-    close_connection(stream);
+    drop(buf_reader);
 }
