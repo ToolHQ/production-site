f82ffd5ebfc0b44d6d5f3c9781da1db818aff38d
Daniel Norio <danieltakasu@gmail.com>
2025-06-09 04:18:48 +0000

    feat(rust-vanilla): Refactor as router init

--- apps/rs-vanilla-back-end/src/main.rs
+++ apps/rs-vanilla-back-end/src/main.rs
@@ -13,58 +13,11 @@ use time::get_current_time_utc_string;
 use uuid::Uuid;
 use web::protocol::HttpStatusCode;
 use web::{
-  protocol::HttpVersion, request::HttpRequest, request::HttpResponse, utils::read_http_request,
+  protocol::HttpVersion, request::HttpRequest, request::HttpResponse, router::Router,
+  utils::read_http_request,
 };
 const MAX_CONCURRENT_CONNECTIONS: usize = 100;
 const SERVER_PORT: u16 = 3000;
-
-fn main() {
-  let time = get_current_time_utc_string();
-  let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENT_CONNECTIONS));
-  let listener = TcpListener::bind(format!("0.0.0.0:{}", SERVER_PORT)).expect("Failed to bind");
-  let bytes_alloc_per_request = std::mem::size_of::<HttpRequest>();
-
-  log_action_info!("Server Start", format!("🟢 Starting server at {}", time).as_str(), {
-    server_start_time: time,
-    server_port: SERVER_PORT,
-    bytes_alloc_per_request: bytes_alloc_per_request,
-    max_concurrency_connections: MAX_CONCURRENT_CONNECTIONS,
-  });
-
-  for stream in listener.incoming() {
-    let stream = match stream {
-      Ok(s) => s,
-      Err(e) => {
-        log_action_error!(
-          "Connection failed",
-          format!("❌ Connection failed: {}", e),
-          {
-              error: e.to_string(),
-          }
-        );
-        continue;
-      }
-    };
-
-    let sem = semaphore.clone();
-    thread::spawn(move || {
-      sem.acquire();
-      handle_client(stream);
-      sem.release();
-    });
-  }
-}
-
-fn close_connection(stream: TcpStream) {
-  if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
-    log_error!(
-      format!("Failed to close connection: {}", e).as_str(),
-      &json_map! {
-          error: e.to_string(),
-      }
-    );
-  }
-}
 
 fn handle_http_health_check(_: &HttpRequest, http_response: &mut HttpResponse) -> Option<()> {
   http_response.set_status_code(HttpStatusCode::Ok);
@@ -88,16 +41,70 @@ fn handle_ndjson_request(_: &HttpRequest, http_response: &mut HttpResponse) -> O
         line.clear();
       }
     }
+    http_response.should_force_close_connection = true;
     return None;
   } else {
-    http_response.set_status_code(HttpStatusCode::InternalServerError);
+    http_response.set_status_code(HttpStatusCode::NotFound);
     http_response.set_header("Content-Type", "text/plain");
     let _ = http_response.write_all(b"File not found");
     return None;
   }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn main() {
+  let time = get_current_time_utc_string();
+  let semaphore = Arc::new(Semaphore::new(MAX_CONCURRENT_CONNECTIONS));
+  let listener = TcpListener::bind(format!("0.0.0.0:{}", SERVER_PORT)).expect("Failed to bind");
+  let bytes_alloc_per_request = std::mem::size_of::<HttpRequest>();
+
+  log_action_info!("Server Start", format!("🟢 Starting server at {}", time).as_str(), {
+    server_start_time: time,
+    server_port: SERVER_PORT,
+    bytes_alloc_per_request: bytes_alloc_per_request,
+    max_concurrency_connections: MAX_CONCURRENT_CONNECTIONS,
+  });
+
+  let router = Router::new()
+    .get("/health", handle_http_health_check)
+    .get("/ndjson", handle_ndjson_request);
+
+  for stream in listener.incoming() {
+    let stream = match stream {
+      Ok(s) => s,
+      Err(e) => {
+        log_action_error!(
+          "Connection failed",
+          format!("❌ Connection failed: {}", e),
+          {
+              error: e.to_string(),
+          }
+        );
+        continue;
+      }
+    };
+
+    let sem = semaphore.clone();
+    let cloned_router = router.clone();
+    thread::spawn(move || {
+      sem.acquire();
+      handle_client(stream, &cloned_router);
+      sem.release();
+    });
+  }
+}
+
+fn close_connection(stream: TcpStream) {
+  if let Err(e) = stream.shutdown(std::net::Shutdown::Both) {
+    log_error!(
+      format!("Failed to close connection: {}", e).as_str(),
+      &json_map! {
+          error: e.to_string(),
+      }
+    );
+  }
+}
+
+fn handle_client(mut stream: TcpStream, router: &Router) {
   let peer_addr = match stream.peer_addr() {
     Ok(addr) => addr,
     Err(_) => return,
@@ -131,40 +138,19 @@ fn handle_client(mut stream: TcpStream) {
 
     let mut http_response = HttpResponse::new(http_request.version, stream.try_clone().unwrap());
 
-    if http_request.is_strict_match("GET", "/health")
-      || http_request.is_strict_match("GET", "/ndjson")
-    {
-      if http_request.is_strict_match("GET", "/health") {
-        handle_http_health_check(&http_request, &mut http_response);
-      } else if http_request.is_strict_match("GET", "/ndjson") {
-        handle_ndjson_request(&http_request, &mut http_response);
-      }
-      let should_force_close_connection = http_request.is_strict_match("GET", "/ndjson");
-      if !http_response.headers_sent {
-        http_response.send_headers();
-      }
-      let ok = http_response.status_code as u16 == 200;
-      let elapsed_time = start_time.elapsed();
-      let elapsed_time_ms = elapsed_time.as_millis();
-      let elapsed_time_str = format!("{}ms", elapsed_time_ms);
-      // Logs request
-      if ok {
-        if http_request.path != "/health" {
-          log_action_info!(
-            "Request Received",
-            format!("{} {}", http_request.method, http_request.path).as_str(),
-            {
-              http_method: http_request.method,
-              http_path: http_request.path,
-              http_peer_addr: http_request.peer_addr,
-              bytes_read: http_request.bytes_read,
-              http_status_code: http_response.status_code as u16,
-              elapsed_time: elapsed_time_str.as_str(),
-            }
-          );
-        }
-      } else {
-        log_action_error!(
+    router.route(&http_request, &mut http_response);
+
+    if !http_response.headers_sent {
+      http_response.send_headers();
+    }
+    let ok = http_response.status_code as u16 == 200;
+    let elapsed_time = start_time.elapsed();
+    let elapsed_time_ms = elapsed_time.as_millis();
+    let elapsed_time_str = format!("{}ms", elapsed_time_ms);
+    // Logs request
+    if ok {
+      if http_request.path != "/health" {
+        log_action_info!(
           "Request Received",
           format!("{} {}", http_request.method, http_request.path).as_str(),
           {
@@ -177,35 +163,25 @@ fn handle_client(mut stream: TcpStream) {
           }
         );
       }
-
-      clear_request_context();
-      if !http_request.should_keep_alive() || should_force_close_connection {
-        close_connection(stream);
-        break;
-      }
     } else {
-      // Send 404 as default
-      http_response.set_status_code(HttpStatusCode::NotFound);
-      http_response.send_headers();
-      let elapsed_time = start_time.elapsed();
-      let elapsed_time_ms = elapsed_time.as_millis();
-      let elapsed_time_str = format!("{}ms", elapsed_time_ms);
       log_action_error!(
         "Request Received",
         format!("{} {}", http_request.method, http_request.path).as_str(),
         {
-          "http_method": http_request.method,
+          http_method: http_request.method,
           http_path: http_request.path,
           http_peer_addr: http_request.peer_addr,
           bytes_read: http_request.bytes_read,
           http_status_code: http_response.status_code as u16,
-          elapsed_time: elapsed_time_str.as_str()
+          elapsed_time: elapsed_time_str.as_str(),
         }
       );
-      if !http_request.should_keep_alive() {
-        close_connection(stream);
-        break;
-      }
+    }
+
+    clear_request_context();
+    if !http_request.should_keep_alive() || http_response.should_force_close_connection {
+      close_connection(stream);
+      break;
     }
   }
 }
