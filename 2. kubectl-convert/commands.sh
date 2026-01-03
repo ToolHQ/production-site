4e1ce49cb95a5c8070ba498825dbe19c32481761
Daniel Norio <danieltakasu@gmail.com>
2026-01-03 14:15:31 +0000

    chore: Remove TCP ingress configuration and kubectl-convert script

--- 2. kubectl-convert/commands.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Tested in 2024-03-24
-# From: https://pet2cattle.com/2022/06/kubectl-convert
-
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl-convert"
-sudo install -o root -g root -m 0755 kubectl-convert /usr/local/bin/kubectl-convert
